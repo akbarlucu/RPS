fffeee7ecd32938bad51ea6ecb96ec68e1d64fb0
akbarlucu <rachmadrizqiakbarsyah@gmail.com>
2024-01-12 07:13:27 +0700

    add main

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//"tutorial/myfunc"
+
+func main() {
+
+	//Rock-Paper-Scissor Game
+	var choices = []string{"Rock, Paper, Scissor"} //Variable choices berisi Rock, Paper, Scissor
+
+	rand.Seed(time.Now().UnixNano()) //Untuk menginisialisasi angka acak secara real time
+
+	userScore := 0
+	computerScore := 0
+
+	fmt.Println("Welcome to Rock-Paper-Scissor Game!") //Print
+
+	for {
+		userChoice := ""
+		fmt.Println("Kamu pilih apa kids? ")
+		fmt.Scanln(&userChoice)
+
+		isValidChoice := false
+
+		var choice string
+
+		for _, choice = range choices {
+			if userChoice == choice {
+				isValidChoice = true
+				break
+			}
+		}
+
+		if !isValidChoice {
+			fmt.Println("Invalid choice, please enter Rock, Paper, or Scissor!")
+			continue
+		}
+
+		computerChoice := choices[rand.Intn(len(choices))] // The range start from 0 and finish on 2. It will not take 3.
+	}
+	// a := 12                    //deklarasi int
+	// b := 18                    //deklarasi int
+	// result := myfunc.Sum(a, b) //hasil deklarasi a dan b
+	// fmt.Println(result)
+
+	//Print Hello World
+	//fmt.Println("Hello World")
+
+	/*Boolean
+	var my_test bool=true
+	fmt.Println(my_test)*/
+
+	/*Integer
+	var my_number int=10
+	fmt.Println(my_number)*/
+
+	/*Array
+	var my_array = [3]int{10, 20, 30}
+	var my_array1 = [3]string{"bonek","viking", "arema"}
+
+	fmt.Println(my_array)
+	fmt.Println(my_array1[1])*/
+
+	// var my_slice []int
+	// var other_slice = []int{1, 2, 3}
+	// fmt.Println(my_slice)
+
+	// my_slice = append(my_slice, other_slice...)
+	// fmt.Println(my_slice)
+
+	// my_slice = append(my_slice, 4)
+	// fmt.Println(my_slice)
+
+	// for i := 0; i <= 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// i := 0
+	// for i < 10 {
+	// 	fmt.Println("ini for loop")
+	// 	i++
+	// }
+	// var my_array = [5]int{1, 2, 3, 4, 5}
+	// index := 10
+	// value := 0
+	// for index, value = range my_array {
+	// 	fmt.Println("index of array: ", index)
+	// 	fmt.Println("value of array: ", value)
+	// }
+
+}
---
+++ myfunc/sample.go
@@ -0,0 +1,6 @@
+package myfunc
+
+func Sum(number1 int, number2 int) int {
+	result := number1 + number2
+	return result
+}
